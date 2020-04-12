36c2b0bd43587f30fc40ea719bb957c41337679f
Ram Regar <shiningram@yahoo.com>
2020-04-11 21:26:25 -0700

    adding mangodb and some tests to add and retrive urls

--- urlapp/server.py
+++ urlapp/server.py
@@ -1,12 +1,43 @@
 
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb://urldb:27017/', connect=False)
+db = client.testdb
 
 @app.route("/")
 def home_test():
     return "Hello Ram"
 
+def check_if_url_is_bad(url : str):
+    if db.test_collection.find({"url": url}).count() > 0:
+        return True
+    return False
+
+@app.route('/add_url_api')
+def insert_mongo_db():
+    url = request.args.get('url', default='', type=str)
+    item = {"url" : url}
+    db.test_collection.insert_one(item)
+    return 'Insert ' + str(item) + ' into MongoDB!'
+
+@app.route('/list_url_api')
+def get_mongo_db():
+    result = []
+    for document in db.test_collection.find():
+        result.append(document)
+    return 'Received \n' + str(result) + '\n from MongoDB\n'
+
+@app.route('/check_url_api')
+def check_url():
+    url = request.args.get('url', default='', type=str)
+    if check_if_url_is_bad(url):
+        return 'Url ' + str(url) + ' is bad\n'
+    else:
+        return 'Url ' + str(url) + ' is safe\n'
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-
