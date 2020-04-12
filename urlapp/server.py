9ff01c80e6dca4409b5642d5c2616f730111a977
Ram Regar <shiningram@yahoo.com>
2020-04-11 18:04:33 -0700

    adding test to make sure that app server is working fine

--- urlapp/server.py
+++ urlapp/server.py
@@ -3,6 +3,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def home_test():
+    return "Hi There, I am alive, I am Ram's app server"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
 
